6d1b41c944c5a8ea5d85363ff5ad5915e1629bd3
agnesvn <agnes.work1112@gmail.com>
2020-05-06 06:22:48 +0700

    upload project vue

---
+++ src/components/ProductSlider.vue
@@ -0,0 +1,243 @@
+<template>
+<!-- Women Banner Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+                    <carousel class="product-slider" :items="3" :nav="false" :autoplay="true" :dots="false">
+
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                            <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                            </router-link>                                
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-2.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                              <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                            </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-3.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                             <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                             </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-4.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                              <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                              </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-5.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                   <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                              <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                              </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-6.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                              <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                              </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                         <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-7.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                              <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                              </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                         <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-8.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                             <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                             </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                         <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/product-9.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                   <router-link to="/product">+ Quick View</router-link>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Coat</div>
+                              <router-link to="/product">
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                              </router-link> 
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- Women Banner Section End -->
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+
+export default {
+    name: 'ProductSlider',
+    components: {
+        carousel
+    }
+    
+}
+</script>
+
+<style scoped>
+.product-item{
+    margin-right: 25px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroSection />
+    <ProductSlider />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import ProductSlider from '@/components/ProductSlider.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroSection,
+    ProductSlider,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
